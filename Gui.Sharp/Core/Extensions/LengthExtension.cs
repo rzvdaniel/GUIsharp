f5f9474d84816819c0858fd54982c03ea438a8b4
Razvan Predescu <rzvdaniel@gmail.com>
2017-07-09 10:14:20 +0100

    Few extensions documentation

--- Gui.Sharp/Core/Extensions/LengthExtension.cs
+++ Gui.Sharp/Core/Extensions/LengthExtension.cs
@@ -4,6 +4,20 @@ namespace Gui.Sharp.Core.Extensions
 {
     public static class LengthExtension
     {
+        /// <summary>
+        /// Converts a CSS value to Length format
+        /// </summary>
+        /// <remarks>
+        /// The VSS value formats:
+        /// Px, Em, Ex, Cm, Mm, In, Pt, Pc, Ch, Rem, Vw, Vh, Vmin, Vmax, Percent
+        /// For a complete list of CSS value formats and their description, see Length.Unit.
+        /// </remarks>
+        /// <param name="cssValue">
+        /// The CSS value to convert
+        /// </param>
+        /// <returns>
+        /// The Length of the CSS value
+        /// </returns>
         public static Length GetLength(this string cssValue)
         {
             Length.TryParse(cssValue, out Length length);
