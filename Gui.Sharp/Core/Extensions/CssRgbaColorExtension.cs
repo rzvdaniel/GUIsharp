85424069770b2970cbefa471d65efe6ec8a57d82
Razvan Predescu <rzvdaniel@gmail.com>
2017-07-10 19:12:14 +0100

    Initialize foreground color

--- Gui.Sharp/Core/Extensions/CssRgbaColorExtension.cs
+++ Gui.Sharp/Core/Extensions/CssRgbaColorExtension.cs
@@ -31,31 +31,45 @@ namespace Gui.Sharp.Core.Extensions
         {
             Color color = defaultColor;
 
-            if (!string.IsNullOrEmpty(rgbaColor))
+            try
             {
-                var separators = new Char[] { CommaSeparator };
+                if (!string.IsNullOrEmpty(rgbaColor))
+                {
+                    var separators = new Char[] { CommaSeparator };
 
-                /// How the regex works: https://stackoverflow.com/a/378447/778863
-                /// \(      # Escaped parenthesis, means "starts with a '(' character"
-                /// (       # Parentheses in a regex mean "put (capture) the stuff in between into the Groups array" 
-                /// [^)]    # Any character that is not a ')' character
-                /// *       # Zero or more occurrences of the aforementioned "non ')' char"
-                /// )       # Close the capturing group
-                ///\)
-                
-                var colorsOnly = Regex.Match(rgbaColor, @"\(([^)]*)\)").Groups[1].Value.Split(separators);
+                    /// How the regex works: https://stackoverflow.com/a/378447/778863
+                    /// \(      # Escaped parenthesis, means "starts with a '(' character"
+                    /// (       # Parentheses in a regex mean "put (capture) the stuff in between into the Groups array" 
+                    /// [^)]    # Any character that is not a ')' character
+                    /// *       # Zero or more occurrences of the aforementioned "non ')' char"
+                    /// )       # Close the capturing group
+                    ///\)
 
-                var red = int.Parse(colorsOnly[0]);
-                var green = int.Parse(colorsOnly[1]);
-                var blue = int.Parse(colorsOnly[2]);
+                    var colorsOnly = Regex.Match(rgbaColor, @"\(([^)]*)\)").Groups[1].Value.Split(separators);
 
-                string alphaString = colorsOnly.Count() == 4 ? colorsOnly[3] : string.Empty;
+                    var colorsCount = colorsOnly.Count();
+                    if (colorsCount < 3 || colorsCount > 4)
+                    {
+                        return defaultColor;
+                    }
 
-                var alpha = !string.IsNullOrEmpty(alphaString) ?
-                    (int)Math.Ceiling(float.Parse(alphaString) * 255) :
-                    FullAlpha;
+                    var red = int.Parse(colorsOnly[0]);
+                    var green = int.Parse(colorsOnly[1]);
+                    var blue = int.Parse(colorsOnly[2]);
 
-                color = new Color(red, green, blue, alpha);
+                    string alphaString = colorsOnly.Count() == 4 ? colorsOnly[3] : string.Empty;
+
+                    var alpha = !string.IsNullOrEmpty(alphaString) ?
+                        (int)Math.Ceiling(float.Parse(alphaString) * 255) :
+                        FullAlpha;
+
+                    color = new Color(red, green, blue, alpha);
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO! Should log the exception.
+                return defaultColor;
             }
 
             return color;
